04954390fb2354fdece34be29414129fbf73bcab
JuanFranciscoGG <123713542+JuanFranciscoGG@users.noreply.github.com>
2023-01-27 01:36:48 -0300

    Create function.js

---
+++ function.js
@@ -0,0 +1,4 @@
+document.getElementById("botonformulario").onclick = function() {
+    console.log("Capturamos el evento click") ;
+    document.getElementById("demo").innerHTML="Su formulario ha sido enviado correctamente";
+}
